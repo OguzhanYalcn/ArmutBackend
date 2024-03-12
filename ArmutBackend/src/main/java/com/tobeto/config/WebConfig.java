5edc99caa1b94d1cc4ebe159f6bc9375eef21151
OguzhanYalcn <oguzhanyalcin3968@gmail.com>
2024-03-12 18:35:01 +0300

    .

--- ArmutBackend/src/main/java/com/tobeto/config/WebConfig.java
+++ ArmutBackend/src/main/java/com/tobeto/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+	// BURASI
 	@Bean
 	WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
